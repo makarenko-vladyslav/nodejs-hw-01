376d2c4760efed04ece660b66342a146cc1164ba
Vladyslav Makarenko <makarenko.vlad.112@gmail.com>
2024-06-03 19:12:53 +0200

    init + generate.js

---
+++ src/scripts/generateContacts.js
@@ -0,0 +1,18 @@
+import { PATH_DB } from '../constants/contacts.js';
+import { faker } from '@faker-js/faker';
+import fs from 'fs/promises';
+
+const generateContacts = async (number) => {
+  const contacts = [];
+
+  for (let i = 0; i < number; i++) {
+    contacts.push({
+      name: faker.person.fullName(),
+      phone: faker.string.octal({ length: 7, prefix: '479-' }),
+      email: faker.internet.email().toLowerCase(),
+      job: faker.person.jobTitle(),
+    });ß
+  }
+};
+
+await generateContacts(5);
